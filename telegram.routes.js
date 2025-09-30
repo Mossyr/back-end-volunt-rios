361d2ee3d95b70a7d671b57b378d3289b0649e13
Mossyr <moacircadmiel51@gmail.com>
2025-09-30 13:43:42 -0300

    Delete telegram.routes.js

--- telegram.routes.js
+++
@@ -1,9 +0,0 @@
-// routes/telegram.routes.js
-const express = require('express');
-const router = express.Router();
-const telegramController = require('../controllers/telegram.controller');
-
-// Rota que o Telegram vai chamar. Precisa ser POST.
-router.post('/webhook', telegramController.handleWebhook);
-
-module.exports = router;
