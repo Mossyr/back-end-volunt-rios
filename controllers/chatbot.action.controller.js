3e5b3c4ac42cc0344eca34858985ce1aa8b443a0
Mossyr <moacircadmiel51@gmail.com>
2025-10-16 18:08:53 -0300

    Update chatbot.action.controller.js

--- controllers/chatbot.action.controller.js
+++ controllers/chatbot.action.controller.js
@@ -3,7 +3,7 @@ const ChatbotService = require('../services/chatbot.service');
 const Usuario = require('../models/usuario.model');
 
 exports.handleAction = async (req, res) => {
-    // Adicionamos 'ministerioId', 'turno' e 'voluntarios' ao destructuring
+    // Adicionamos 'ministerioId', 'turno' e 'voluntarios' para receber os dados da conversa
     const { action, turnoId, voluntarioId, data, ministerioId, turno, voluntarios } = req.body;
     
     try {
@@ -11,12 +11,22 @@ exports.handleAction = async (req, res) => {
         let responsePayload;
 
         switch (action) {
-            // --- AÇÕES EXISTENTES ---
-            case 'PROXIMA_ESCALA': /* ...código existente... */ break;
-            case 'ESCALAS_MES': /* ...código existente... */ break;
-            case 'SOLICITAR_TROCA': /* ...código existente... */ break;
-            case 'CONFIRMAR_TROCA': /* ...código existente... */ break;
-            case 'SET_UNAVAILABLE': /* ...código existente... */ break;
+            // AÇÕES EXISTENTES
+            case 'PROXIMA_ESCALA':
+                responsePayload = { type: 'message', reply: await ChatbotService.getProximaEscala(usuarioLogado) };
+                break;
+            case 'ESCALAS_MES':
+                responsePayload = { type: 'message', reply: await ChatbotService.getEscalasDoMes(usuarioLogado) };
+                break;
+            case 'SOLICITAR_TROCA':
+                responsePayload = await ChatbotService.prepararTrocaDeEscala(usuarioLogado);
+                break;
+            case 'CONFIRMAR_TROCA':
+                responsePayload = { type: 'message', reply: await ChatbotService.iniciarTrocaComTelegram(usuarioLogado, turnoId, voluntarioId) };
+                break;
+            case 'SET_UNAVAILABLE':
+                responsePayload = { type: 'message', reply: await ChatbotService.setUnavailableFromBot(usuarioLogado, data) };
+                break;
 
             // --- NOVO FLUXO DE CRIAÇÃO DE ESCALA ---
             case 'CRIAR_ESCALA_INICIAR':
@@ -24,29 +34,24 @@ exports.handleAction = async (req, res) => {
                 break;
 
             case 'CRIAR_ESCALA_PEDIR_DATA':
-                // O usuário escolheu o ministério, agora o bot pede a data.
                 responsePayload = {
                     type: 'date_picker_creation',
                     reply: 'Ótima escolha! Agora, por favor, selecione a data para a nova escala.',
-                    // Passamos o ministerioId para a próxima etapa
                     context: { ministerioId: ministerioId }
                 };
                 break;
 
             case 'CRIAR_ESCALA_PEDIR_TURNO':
-                // O usuário escolheu a data, agora o bot pede o turno.
                 responsePayload = await ChatbotService.pedirTurno(ministerioId, data);
                 break;
 
             case 'CRIAR_ESCALA_PEDIR_VOLUNTARIOS':
-                // O usuário escolheu o turno, agora o bot pede os voluntários.
-                // Passamos todo o contexto para a próxima etapa.
                 responsePayload = await ChatbotService.getVoluntariosParaCriacao(ministerioId, data);
+                // Adiciona o contexto para o próximo passo
                 responsePayload.context = { ministerioId, data, turno };
                 break;
             
             case 'CRIAR_ESCALA_CONFIRMAR':
-                // O usuário confirmou os voluntários, agora criamos a escala.
                 const confirmMsg = await ChatbotService.criarEscalaPeloChat(usuarioLogado.id, ministerioId, data, turno, voluntarios);
                 responsePayload = { type: 'message', reply: confirmMsg };
                 break;
@@ -60,6 +65,3 @@ exports.handleAction = async (req, res) => {
         res.status(500).json({ type: 'message', reply: 'Ocorreu um erro interno.' });
     }
 };
-
-// Bloco para manter as ações existentes funcionando
-exports.handleAction.case = (action, handler) => { /* ... */ };
